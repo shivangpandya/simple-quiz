13554f5c55f6b38563078c0bfa30611fa5e93ace
Shivang Pandya <shivang.pandya01@gmail.com>
2018-03-07 20:00:37 +0530

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,50 @@
+function submitAnswers()
+{
+	var total = 5;
+	var score = 0;
+	
+
+	
+	// Get user input
+	var q1 = document.forms["quizForm"]["q1"].value;
+	var q2 = document.forms["quizForm"]["q2"].value;
+	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+
+// 	//validation
+
+var i;
+
+	for ( i =1;i<=total; i++) 
+	{
+
+		if( eval('q'+i ) == null || eval('q'+i ) == '')
+		{
+			alert('You missed Question '+ i);
+			return false;
+		}
+	}
+
+
+	//Set correct answers
+
+	var answers = ["a","b","c","b","b"];
+
+	//Check answers
+
+
+	for (var i = 1; i < total ; i++) 
+	{
+		if(eval('q'+i ) == answers[i-1]){
+		score++;
+	}
+  }
+
+	// var results = document.getElementsById('results'); 
+	// results.innerHTML = '<h3>You scored <span>'+score+'</span>out of<span>'+total+'</span></h3>';
+
+	alert('You scored '+score+'out of '+total);
+return false;
+
+}
